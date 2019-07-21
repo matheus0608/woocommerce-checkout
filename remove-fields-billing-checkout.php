4b1b45602c7ab2ec56fc45d94fb025f51a65d9f6
Matheus Santos <matheusmqd@gmail.com>
2019-07-21 11:18:19 -0300

    1.2

---
+++ remove-fields-billing-checkout.php
@@ -0,0 +1,9 @@
+<?php 
+
+function prefix_woocommerce_checkout_fields( $fields ) {
+    unset( $fields['billing']['billing_cpf'] );
+    unset( $fields['billing']['billing_neighborhood'] );
+
+	return $fields; 
+}
+add_filter( 'woocommerce_checkout_fields' , 'prefix_woocommerce_checkout_fields' );
