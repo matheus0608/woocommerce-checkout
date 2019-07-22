1450871ddb8f273151dd1a2d8828f6c7ff3da0c7
Matheus Santos <matheusmqd@gmail.com>
2019-07-22 05:12:39 -0300

    1.6

--- add-fields-billing-checkout.php
+++ add-fields-billing-checkout.php
@@ -5,6 +5,7 @@
  * Source: https://docs.woocommerce.com/document/tutorial-customising-checkout-fields-using-actions-and-filters/
  */
 function prefix_woocommerce_checkout_fields( $fields ) {
+    // add field type text
     $fields['billing']['billing_cpf'] = array(
         'label' => __( 'CPF' ),
         'placeholder' => '',
@@ -17,16 +18,21 @@ function prefix_woocommerce_checkout_fields( $fields ) {
         'priority' => 23, // position in checkout
     );
 
-    $fields['billing']['billing_neighborhood'] = array(
-        'label' => __( 'neighborhood' ),
+    // add field type select
+    $fields['billing']['billing_sex'] = array(
+        'label' => __( 'Type Sex' ),
         'placeholder' => '',
         'required' => false, 
         'clear' => false, 
-        'type' => 'text', 
+        'type' => 'select', 
         'input_class' => array( '' ), // add class in tag input
         'label_class' => array( '' ), // add class in tag label
         'class' => array( '' ), //form-row-first, form-row-last, form-row-wide or custom class, add class in tag p
         'priority' => 23, // position in checkout
+        'options' => array(
+            'female' => 'Female', 
+            'male' => 'Male', 
+        );
     );
     
     return $fields; 
